de59fc90380374413c949f36ce7dde24ba5543c8
DiemNguyen1609 <50823647+DiemNguyen1609@users.noreply.github.com>
2021-03-14 16:54:45 +0700

    add splash screen

--- data/src/main/java/com/example/api/MovieApi.kt
+++ data/src/main/java/com/example/api/MovieApi.kt
@@ -4,8 +4,6 @@ import io.reactivex.Flowable
 import retrofit2.http.*
 
 interface MovieApi {
-   // https://api.themoviedb.org/3/discover/movie?api_key=1f5b86b5d2ec241ec5863bc7941fad12&language=en-US&sort_by=popularity.desc&page=1
     @GET("movie?api_key=1f5b86b5d2ec241ec5863bc7941fad12&language=en-US&sort_by=popularity.desc&page=1")
     fun getDiscover(): Flowable<String>
-
 }
