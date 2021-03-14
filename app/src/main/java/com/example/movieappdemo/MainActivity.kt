8dc50a3ed8072b2fdeaafd8bbde09bacb967a70f
DiemNguyen1609 <50823647+DiemNguyen1609@users.noreply.github.com>
2021-03-14 22:53:33 +0700

    updating menu

--- app/src/main/java/com/example/movieappdemo/MainActivity.kt
+++ app/src/main/java/com/example/movieappdemo/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.movieappdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel : MovieViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel.getDiscover()
+        changeStatusBarColor(R.color.transparent_color)
     }
 
-    private val viewModel : MovieViewModel by viewModel()
+    private fun changeStatusBarColor(colorRes: Int) {
+        val window = this.window
+        window.statusBarColor = ContextCompat.getColor(this , colorRes)
+    }
+
+
 
 
 }
--- common/src/main/java/com/example/common/BaseFragment.kt
+++ common/src/main/java/com/example/common/BaseFragment.kt
@@ -1,4 +1,4 @@
 package com.example.common
 
-class kkk {
+class BaseFragment {
 }
